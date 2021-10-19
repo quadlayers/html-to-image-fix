d73676dbf76ffb5ba547e82e79ec08a2e18090b8
fedev21 <60235004+fedev21@users.noreply.github.com>
2021-10-19 10:18:26 -0300

    update html-to-image-fix

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './ui/components/layout/App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
